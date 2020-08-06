571fdc60a0efbda1a35d24e4be13a7ca740b5065
Brandon Gonzalez <bjgman38@gmail.com>
2020-08-06 17:26:09 -0600

    fooling with css

--- js/app.js
+++ js/app.js
@@ -57,11 +57,13 @@ var Lima = {
 
 function siteSales(siteArr) {
   var hourSales = 0;
+  var totalSales = 0;
   // calculate and store simulated amounts of cookies purchased for each hour 
   for (var i = 0; i <= 13; i++) {
     var hourlyCustomers = (Math.floor(Math.random() * (siteArr.maxCustomer - siteArr.minCustomer)) + siteArr.minCustomer);
     hourSales = hourlyCustomers * siteArr.avgSale;
     siteArr.salesSum += hourlyCustomers * siteArr.avgSale;
+    totalSales += hourSales;
     //store result in an array
     siteArr.salesActual.push(parseInt(hourSales));
   }
@@ -72,23 +74,30 @@ function siteSales(siteArr) {
   //create
   var newSite = document.createElement('div');
   var siteName = document.createElement('h2');
-  siteName.textContent =`${siteArr.name} Sales Data`;
+  siteName.textContent = `${siteArr.name} Sales Data`;
   var salesList = document.createElement('ul');
 
   //append
   siteContainer.append(newSite);
   newSite.append(siteName);
   newSite.append(salesList);
+  for (i = 0; i <= siteArr.salesActual.length; i++) {
+    if (i < siteArr.salesActual.length) {
+      var salesItem = document.createElement('li');
+      salesItem.textContent = `${siteArr.hoursOfOperation[i]}: ${siteArr.salesActual[i]} Cookies`;
+      salesList.append(salesItem);
+    } else {
+      salesItem = document.createElement('li');
+      salesItem.textContent = `Total: ${parseInt(totalSales)} Cookies`;
+      salesList.append(salesItem);
 
-  for (i = 0; i < siteArr.salesActual.length; i++) {
-    var salesItem = document.createElement('li');
-    salesItem.textContent = `${siteArr.hoursOfOperation[i]}: ${siteArr.salesActual[i]} cookies`;
-    salesList.append(salesItem);
+    }
+
   }
 }
 
-var Sites = [Seattle,Tokyo,Dubai,Paris,Lima];
+var Sites = [Seattle, Tokyo, Dubai, Paris, Lima];
 
-for (var i = 0; i < Sites.length ; i ++) {
+for (var i = 0; i < Sites.length; i++) {
   siteSales(Sites[i]);
 }
