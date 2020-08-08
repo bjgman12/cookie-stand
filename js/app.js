b64047a54627fcf9473b1505126816e95a7b2951
Brandon Gonzalez <bjgman38@gmail.com>
2020-08-08 09:53:26 -0600

    made hours a global variable due to it being encourage heavily

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 // make object
 var Seattle = {
   name: 'Seattle',
@@ -7,7 +8,6 @@ var Seattle = {
   maxCustomer: 65,
   avgSale: 6.3,
   salesActual: [],
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   salesSum: 0
 
 };
@@ -18,7 +18,7 @@ var Tokyo = {
   maxCustomer: 24,
   avgSale: 1.2,
   salesActual: [],
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+
   salesSum: 0
 
 };
@@ -28,7 +28,6 @@ var Dubai = {
   maxCustomer: 38,
   avgSale: 3.7,
   salesActual: [],
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   salesSum: 0
 
 };
@@ -39,7 +38,6 @@ var Paris = {
   maxCustomer: 38,
   avgSale: 2.3,
   salesActual: [],
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   salesSum: 0
 
 };
@@ -50,7 +48,6 @@ var Lima = {
   maxCustomer: 16,
   avgSale: 4.6,
   salesActual: [],
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   salesSum: 0
 
 };
@@ -59,7 +56,7 @@ function compileSiteSales(siteArr) {
   var hourSales = 0;
   var totalSales = 0;
   // calculate and store simulated amounts of cookies purchased for each hour 
-  for (var i = 0; i < siteArr.hoursOfOperation.length; i++) {
+  for (var i = 0; i < hoursOfOperation.length; i++) {
     var hourlyCustomers = (Math.floor(Math.random() * (siteArr.maxCustomer - siteArr.minCustomer)) + siteArr.minCustomer) + 1;
     hourSales = hourlyCustomers * siteArr.avgSale;
     siteArr.salesSum += hourlyCustomers * siteArr.avgSale;
@@ -67,16 +64,13 @@ function compileSiteSales(siteArr) {
     //store result in an array
     siteArr.salesActual.push(parseInt(hourSales));
   }
-
   //target
   var siteContainer = document.getElementById('siteContainer');
-
   //create
   var newSite = document.createElement('div');
   var siteName = document.createElement('h2');
   siteName.textContent = `${siteArr.name} Sales Data`;
   var salesList = document.createElement('ul');
-
   //append
   siteContainer.append(newSite);
   newSite.append(siteName);
@@ -84,15 +78,13 @@ function compileSiteSales(siteArr) {
   for (i = 0; i <= siteArr.salesActual.length; i++) {
     if (i < siteArr.salesActual.length) {
       var salesItem = document.createElement('li');
-      salesItem.textContent = `${siteArr.hoursOfOperation[i]}: ${siteArr.salesActual[i]} Cookies`;
+      salesItem.textContent = `${hoursOfOperation[i]}: ${siteArr.salesActual[i]} Cookies`;
       salesList.append(salesItem);
     } else {
       salesItem = document.createElement('li');
       salesItem.textContent = `Total: ${parseInt(totalSales)} Cookies`;
       salesList.append(salesItem);
-
     }
-
   }
 }
 for (var i = 0; i < Sites.length; i++) {
